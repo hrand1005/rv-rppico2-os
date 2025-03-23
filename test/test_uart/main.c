6c6209e6dc759558aabd9668dbd5147232016a88
Herbie Rand <hjrand1005@gmail.com>
2025-03-23 16:35:10 -0700

    Rewrite UART test with sensible output

--- test/test_uart/main.c
+++ test/test_uart/main.c
@@ -1,18 +1,71 @@
+/**
+ * @brief Tests UART with mtimer interrupt.
+ *
+ * The expected behavior is to see a new 'tick' (integer) each second.
+ * Each tick is printed on a new line in the minicom console.
+ * Tick overflows after max uint32 size.
+ *
+ * @author Herbie Rand
+ */
 #include "asm.h"
 #include "clock.h"
+#include "mtime.h"
 #include "resets.h"
 #include "types.h"
 #include "uart.h"
+
+void print_tick();
+
+static uint32_t tick = 0;
+static uint32_t us = 10000000;
+static char buf[10];
 
 int main() {
     initial_reset_cycle();
     clock_defaults_set();
     postclk_reset_cycle();
     uart_init();
+    mtimer_enable();
+
+    if (mtimer_start(us)) {
+        asm volatile("ebreak");
+    }
+
     while (1) {
-        uart_putc('X');
-        for (uint32_t i = 0; i < 100000; i++)
-            ;
+        asm volatile("wfi");
     }
     return 0;
+}
+
+void print_tick() {
+    int32_t i = 0;
+    uint32_t num = tick;
+
+    if (num == 0) {
+        buf[i++] = 0 + '0';
+    } else {
+        while (num > 0) {
+            buf[i++] = (num % 10) + '0';
+            num = num / 10;
+        }
+    }
+
+    while (i < 10) {
+        buf[i++] = 0;
+    }
+
+    uart_putc('\n');
+    uart_putc('\r');
+    while (--i >= 0) {
+        if (buf[i] == 0) {
+            continue;
+        }
+        uart_putc(buf[i]);
+    }
+}
+
+void isr_mtimer_irq() {
+    mtimer_start(us);
+    tick++;
+    print_tick();
 }
